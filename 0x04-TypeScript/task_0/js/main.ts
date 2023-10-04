daedb11673448e647977d56d2babd847137e3a0f
Angela <kafuigalley7@gmail.com>
2023-10-04 22:34:31 +0000

    initial

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,43 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 20,
+  location: 'New York'
+};
+
+const student2: Student = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  age: 22,
+  location: 'Los Angeles'
+};
+
+const studentsList: Student[] = [student1, student2];
+
+function renderTable() {
+  const table = document.createElement('table');
+  
+  studentsList.forEach(student => {
+    const row = document.createElement('tr');
+    const firstNameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+    
+    firstNameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+    
+    table.appendChild(row);
+  });  
+  document.body.appendChild(table);
+}
+
+renderTable();
